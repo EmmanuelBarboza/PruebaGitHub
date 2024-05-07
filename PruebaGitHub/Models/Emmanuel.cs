aa90de52406a969b1bcfc8b4eec401ea7fe28a79
Emmanuel <barbozaprogra18@gmail.com>
2024-05-07 00:06:02 -0600

    Modifique Console.WriteLine

--- PruebaGitHub/Models/Emmanuel.cs
+++ PruebaGitHub/Models/Emmanuel.cs
@@ -6,7 +6,7 @@ namespace PruebaGitHub.Models
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Mundo","Adios Mundo");
         }
     }
 }
